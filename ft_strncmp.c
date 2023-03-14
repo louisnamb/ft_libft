217a977821067bdd944de83be74e17341a3c4579
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-03-14 16:15:53 +1030

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,24 +16,38 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int	count;
 
 	count = 0;
-	if (n >= 0)
+	while (n-- && s1 && s2)
 	{
-		return (0);
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
-	while (count < n -1 && s1[count] == s2[count] && !s1[count] && !s2[count])
-	{
-		count++;
-	}
-	return (s1[count] - s2[count]);
+	return (0);
 }
-
 
 #include <stdio.h>
 #include <string.h>
 int main()
 {
 	int d = 5;
-	printf("real: %d\n", strncmp("test", "testss", 7));
-	printf("ft: %d\n", ft_strncmp("test", "testss", 7));
+	int p;
+	int i = 0;
+	int j;
+
+	size_t numarr[4] = {7, 7, 4, 4};
+	const char s1arr[4][7] = {"test", "testss", "test", ""};
+	const char s2arr[4][7] = {"testss", "test", "tEst", "test"};
+	while (i < 4)
+	{
+		p = strncmp(s1arr[i], s2arr[i], numarr[i]);
+		j = ft_strncmp(s1arr[i], s2arr[i], numarr[i]);
+		if (p == j)
+			printf("equal %d\n", p);
+		else
+			printf("not equal: %d != %d\n", p, j);
+		i++;		
+	}
+	
 	return (0);
 }
